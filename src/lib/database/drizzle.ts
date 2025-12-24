82e715e6faac42fc339e3c6b1f8ddabac49c0431
Nico Swan <hi@nicoswan.com>
2025-12-24 19:38:07 +0200

    chore: disable database SSL connection and add production TODO

--- src/lib/database/drizzle.ts
+++ src/lib/database/drizzle.ts
@@ -22,7 +22,7 @@ const pool = new Pool({
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
-  ssl: getSSLConfig()
+  ssl: false // TODO: Enable SSL in production
 })
 
 export const db = drizzle(pool, { schema })
